2bc5541bebf45bc3b45ddb95fff4b05095f628ff
sfx09 <aleth@tuta.io>
2024-08-11 21:12:46 +0530

    feat: design event loop

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type command struct {
+	name     string
+	desc     string
+	callback func() error
+}
+
+func eval(s string) {
+	switch s {
+	case "help":
+		fmt.Println("Display help message")
+	default:
+		fmt.Println("Display help message")
+	}
+}
+
+func eventloop(eval func(string)) {
+	sc := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("> ")
+		sc.Scan()
+		op := sc.Text()
+		eval(op)
+	}
+}
 
 func main() {
-	fmt.Println("PokeCLI")
+	eventloop(eval)
 }
