16f8bf5b3f90571182c47b45eb427b17380737f7
sfx09 <aleth@tuta.io>
2024-08-12 09:35:29 +0530

    feat: add command to list pokemon location areas

--- commands.go
+++ commands.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 )
 
-func Eval(args ...string) {
+func NewCommandEvalutor() func(...string) {
 	cmds := getCommands()
-	cmd, exists := cmds[args[0]]
-	if !exists {
-		cmds["help"].execute()
-		return
+	return func(args ...string) {
+		cmd, exists := cmds[args[0]]
+		if !exists {
+			cmds["help"].execute()
+			return
+		}
+		cmd.execute(args[1:]...)
 	}
-	cmd.execute(args[1:]...)
 }
 
 type command struct {
@@ -33,6 +37,11 @@ func getCommands() map[string]command {
 			Desc:    "Exit the program",
 			execute: exitCommand,
 		},
+		"map": {
+			Name:    "map",
+			Desc:    "List Pokemon locations",
+			execute: NewMapCommand(),
+		},
 	}
 }
 
@@ -48,4 +57,32 @@ func exitCommand(args ...string) error {
 	fmt.Println("Exiting...")
 	os.Exit(0)
 	return nil
+}
+
+func NewMapCommand() func(args ...string) error {
+	type Response struct {
+		Previous string
+		Next     string
+		Results  []struct {
+			Name string `json:"name"`
+			Url  string `json:"url"`
+		} `json:"results"`
+	}
+	resp := Response{}
+	url := "https://pokeapi.co/api/v2/location/"
+	return func(args ...string) error {
+		r, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		err = json.NewDecoder(r.Body).Decode(&resp)
+		if err != nil {
+			return err
+		}
+		for _, city := range resp.Results {
+			fmt.Println(city.Name)
+		}
+		url = resp.Next
+		return nil
+	}
 }
--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	EventLoop(Eval)
+	EventLoop(NewCommandEvalutor())
 }
